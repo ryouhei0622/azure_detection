b36ac7fe9fbe6811ae8c86162e10a9a9eb7f225c
ryouhei0622 <seijakunomutou@gmail.com>
2023-05-21 01:14:46 +0900

    add: add functions

---
+++ detect_tags.py
@@ -0,0 +1,36 @@
+import json
+import os
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+
+with open("secret.json") as f:
+    secret = json.load(f)
+KEY = secret["KEY"]
+ENDPOINT = secret["ENDPOINT"]
+client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
+
+
+def get_tags(file_path: str) -> list[str]:
+    tag_list = [str]
+    local_image = open(file_path, "rb")
+    image_analysis = client.analyze_image_in_stream(local_image, visual_features=[VisualFeatureTypes.tags])
+
+    for tag in image_analysis.tags:
+        tag_list.append(tag.name)
+    return tag_list
+
+
+def detect_objects(file_path: str) -> list:
+    local_image = open(file_path, "rb")
+    detected_objects_result = client.detect_objects_in_stream(local_image, visual_features=[VisualFeatureTypes.objects])
+    objects = detected_objects_result.objects
+    return objects
+
+
+local_image_path = "/Users/a81701/code/streamlit/detection/02_物体検出アプリ/sample01.jpg"
+# print(get_tags(local_image_path))
+
+l = detect_objects(local_image_path)
+print(l[0].object_property)
